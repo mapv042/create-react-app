0a9cd7b2d9da2dde33ab8dded8121f8b2f2e1b0d
Miguel Angel Partida Velasco <mapv04@Miguels-MacBook-Pro.local>
2024-02-20 00:17:54 -0600

    new data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello from Render!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div class= "body">
+      <div class="heart">
+        </div>
+        <div class="contenido"><h1>Buenos Días Annie</h1></div>
+        </div>
     );
   }
 }
