0e3eaa25039df4585dafb1ed17cbd4b1004e242e
Miguel Angel Partida Velasco <mapv04@Miguels-MacBook-Pro.local>
2024-02-21 16:31:14 -0600

    add one more page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import React, { Component } from 'react';
-import './styles.css';
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+import AppFlowers from "./AppFlowers";
 
-class App extends Component {
-  render() {
-    return (
-      <div class= "body">
-      <div class="heart">
-        </div>
-        <div class="contenido"><h1>Buenos Días Annie</h1></div>
-        </div>
-    );
+const GoodMorning = (props) => {
+  const today = new Date();
+  const curHr = today.getHours();
+
+  let welcome = "Buenos Días";
+
+  if (curHr < 12) {
+    welcome = "Buenos Días";
+  } else if (curHr < 18) {
+    welcome = "Buenas Días";
+  } else {
+    welcome = "Buenas Noches";
   }
-}
+  return (
+    <div class="body" onClick={() => props.setState(props.state + 1)}>
+      <div class="big-heart"></div>
+      <div class="contenido">
+        <h1>{welcome} Annie</h1>
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  const [state, setState] = useState(0);
+
+  useEffect(() => {
+    console.log("state: ", state);
+  }, [state]);
+
+  if (state === 0) {
+    return <GoodMorning state={state} setState={setState} />;
+  } else {
+    return <AppFlowers />;
+  }
+};
 
 export default App;
